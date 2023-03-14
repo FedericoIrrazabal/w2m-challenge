bfa997575ab6b24433d458f1d6e08f80719ff35b
Federico Irrazabal <fnirrazabal@gmail.com>
2023-03-14 13:47:57 -0300

    add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoutesModule } from './routes/routes.module';
+import { LayoutModule } from './layout/layout.module';
 
 
 
@@ -24,6 +25,7 @@ import { RoutesModule } from './routes/routes.module';
     BrowserAnimationsModule,
     RoutesModule,
     HttpClientModule,
+    LayoutModule
 
   ],
   providers: [],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class LayoutModule { }
